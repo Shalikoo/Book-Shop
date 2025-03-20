3a7810f02408b22566420db018e04b3f8c2e48d3
Shalev Azulay <shalev3238@gmail.com>
2025-03-20 16:06:29 +0200

    Init commit

---
+++ js/book-controller.js
@@ -0,0 +1,21 @@
+'use strict'
+
+function onInit() {
+    renderBooks()
+}
+
+function renderBooks() {
+    var books = getBooks()
+    var strHTMLs = books.map(book => `
+        <tr>
+            <td>${book.title}</td>
+            <td>${book.price}</td>
+            <td>
+                <button onclick="onReadBook('${book.id}')">Read</button>
+                <button onclick="onUpdateBook('${book.id}')">Update</button>
+                <button onclick="onRemoveBook('${book.id}')">Delete</button>
+            </td>
+        </tr>
+        `)
+        document.querySelector('tbody').innerHTML = strHTMLs.join('')
+}
---
+++ js/service/book-service.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+var gBooks = [
+    { id: 'bg4J78', title: 'The Adventures of Lori Ipsi', price: 120, imgUrl: 'lori-ipsi.jpg' },
+    { id: 'gh6K89', title: 'JavaScript for Beginners', price: 95, imgUrl: 'js-beginners.jpg' }
+];
+
+function getBooks() {
+    return gBooks;
+}
